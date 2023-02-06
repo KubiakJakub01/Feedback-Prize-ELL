5a786194920526eddc3bf0688ee69e00e2e808bf
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-06 20:54:49 +0100

    Config logger

--- src/train.py
+++ src/train.py
@@ -7,9 +7,21 @@ import argparse
 import os
 import sys
 import time
+import logging
 
 # Import torch
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+
+# Set up logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    handlers=[logging.StreamHandler(sys.stdout)])
+logger = logging.getLogger(__name__)           
+
+
+if __name__ == "__main__":
+    pass
