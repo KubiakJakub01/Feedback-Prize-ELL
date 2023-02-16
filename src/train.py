f752c91c592ac5ce514127569e7ac5452728cbcb
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-16 22:36:10 +0100

    Refactor code

--- src/train.py
+++ src/train.py
@@ -19,24 +19,35 @@ from torch.utils.data import DataLoader
 from transformers import BertTokenizer, BertModel, BertForSequenceClassification
 
 # Set up logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    handlers=[logging.StreamHandler(sys.stdout)])
-logger = logging.getLogger(__name__)           
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+logger = logging.getLogger(__name__)
 
 
 def get_args():
     """
     Get command line arguments.
     """
-    parser = argparse.ArgumentParser(description='Train a model')
-    parser.add_argument('--mode_path', type=str, default='bert-base-uncased',
-                        help='Path to BERT model')
-    parser.add_argument('--train_data_path', type=str, default='data/train.csv',
-                        help='Path to training data')
-    parser.add_argument('--valid_data_path', type=str, default='data/valid.csv',
-                        help='Path to validation data')
+    parser = argparse.ArgumentParser(description="Train a model")
+    parser.add_argument(
+        "--mode_path", type=str, default="bert-base-uncased", help="Path to BERT model"
+    )
+    parser.add_argument(
+        "--train_data_path",
+        type=str,
+        default="data/train.csv",
+        help="Path to training data",
+    )
+    parser.add_argument(
+        "--valid_data_path",
+        type=str,
+        default="data/valid.csv",
+        help="Path to validation data",
+    )
     return parser.parse_args()
 
 
