c30b230c374be32dd7cc717575e71209caf47138
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-20 22:54:36 +0100

    Create TextDataset class

--- src/utils/data.py
+++ src/utils/data.py
@@ -11,4 +11,55 @@ import pandas as pd
 import numpy as np
 
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+
+# Set up logging
+logger = logging.getLogger(__name__)
+
+class TextDataset(Dataset):
+    """
+    Dataset for text classification.
+    """
+
+    def __init__(self, data_path, tokenizer, numberic_col_list, max_len=128):
+        """
+        Args:
+            data_path (str): Path to data.
+            tokenizer (BertTokenizer): Tokenizer for encoding text.
+            max_len (int): Maximum length of a sequence.
+        """
+        self.data = pd.read_csv(data_path)
+        self.tokenizer = tokenizer
+        self.numberic_col_list = numberic_col_list.copy()
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int): Index of item to return.
+
+        Returns:
+            dict: Dictionary containing input_ids, attention_mask, and labels.
+        """
+        text = self.data.loc[idx, "full_text"]
+        labels = self.data.loc[idx, self.numberic_col_list]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            padding="max_length",
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
+
+        return {
+            "text": text,
+            "input_ids": encoding["input_ids"].flatten(),
+            "attention_mask": encoding["attention_mask"].flatten(),
+            "labels": torch.tensor(labels, dtype=torch.float),
+        }
