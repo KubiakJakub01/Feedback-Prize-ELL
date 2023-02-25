dff69305a1d157162eb056787bbc4679690bf8dd
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-25 19:50:02 +0100

    Init get_optimizer function

--- src/utils/training.py
+++ src/utils/training.py
@@ -13,3 +13,17 @@ import torch.optim as optim
 
 # Set up logging
 logger = logging.getLogger(__name__)
+
+
+def get_optimizer(model, lr):
+    """
+    Get optimizer.
+
+    Args:
+        model (nn.Module): Model to optimize.
+        lr (float): Learning rate.
+
+    Returns:
+        Optimizer: PyTorch optimizer.
+    """
+    return optim.Adam(model.parameters(), lr=lr)
