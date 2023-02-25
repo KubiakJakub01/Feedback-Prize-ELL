cdf21d950e65f5adfd9ed1564d36b9110f8f8b7c
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-25 20:14:20 +0100

    Init fit function with train one epoch and valid one epoch

--- src/utils/training.py
+++ src/utils/training.py
@@ -78,6 +78,7 @@ class Trainer:
     device: torch.device
     save_path: Path
     num_epochs: int
+    validation_step: int
     num_training_steps: int
     num_warmup_steps: int
     log_step: int
@@ -91,8 +92,157 @@ class Trainer:
         # Initialize tensorboard writer
         self.writer = SummaryWriter(log_dir=self.save_path / "tensorboard")
 
-        # Initialize global step
+        # Initialize training variables
         self.global_step = 0
+        self.trai_loss = 0.0
+        self.valid_loss = 0.0
+
+
+    def process_batch(self, batch):
+        """
+        Process batch.
+
+        Args:
+            batch (dict): Batch dictionary.
+
+        Returns:
+            tuple: Tuple of input ids, attention mask, token type ids, and labels.
+        """
+        # Get batch
+        input_ids = batch["input_ids"].to(self.device)
+        attention_mask = batch["attention_mask"].to(self.device)
+        token_type_ids = batch["token_type_ids"].to(self.device)
+        labels = batch["labels"].to(self.device)
+
+        return input_ids, attention_mask, token_type_ids, labels
+
+    @torch.no_grad()
+    def valid_one_step(self, batch):
+        """
+        Method for validating model for one step.
+
+        Args:
+            batch (dict): Batch dictionary.
+
+        Returns:
+            float: Validation loss.
+        """
+        # Get batch
+        input_ids, attention_mask, token_type_ids, labels = self.process_batch(batch)
+
+        # Get model outputs
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+        logits = outputs[0]
+
+        # Calculate loss
+        loss = self.loss_fn(logits, labels)
+
+        return loss.item()
+
+    def valid_one_epoch(self):
+        """
+        Method for validating model for one epoch.
+
+        Returns:
+            float: Validation loss.
+        """
+        if self.global_step % self.validation_step != 0:
+            return
+
+        # Set model to evaluation mode
+        self.model.eval()
+
+        # Initialize loss
+        loss = 0
+
+        # Iterate over batches
+        for batch in self.valid_data_loader:
+            # Validate model for one step
+            loss += self.valid_one_step(batch)
+
+        # Calculate average loss
+        loss /= len(self.valid_data_loader)
+
+        return loss
+
+    def train_one_step(self, batch):
+        """
+        Method for training model for one step.
+
+        Args:
+            batch (dict): Batch dictionary.
+
+        Returns:
+            float: Training loss.
+        """
+        # Get batch
+        input_ids, attention_mask, token_type_ids, labels = self.process_batch(batch)
+
+        # Get model outputs
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+        logits = outputs[0]
+
+        # Calculate loss
+        loss = self.loss_fn(logits, labels)
+
+        # Backpropagate loss
+        loss.backward()
+
+        # Clip gradients
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+
+        # Update optimizer
+        self.optimizer.step()
+
+        # Update scheduler
+        self.scheduler.step()
+
+        # Update global step
+        self.global_step += 1
+
+        return loss.item()
+
+    def train_one_epoch(self):
+        """
+        Method for training model for one epoch.
+        """
+        self.model.train()
+        for batch in self.train_data_loader:
+            # Train model for one step
+            loss = self.train_one_step(batch)
+
+            # Log training loss
+            if self.global_step % self.log_step == 0:
+                logger.info(
+                    "Step {}/{}: Loss: {}".format(
+                        self.global_step,
+                        self.num_training_steps,
+                        loss.item(),
+                    )
+                )
+                self.writer.add_scalar(
+                    "training/loss", loss.item(), self.global_step
+                )
+
+            # Save model
+            if self.global_step % self.save_step == 0:
+                logger.info("Saving model")
+                self.model.save_pretrained(self.save_path)
+            
+            # Update validation loss
+            valid_loss = self.valid_one_epoch()
+
+            # Update global step
+            self.global_step += 1
+
 
     def fit(self):
         """
@@ -102,4 +252,3 @@ class Trainer:
         for epoch in range(self.num_epochs):
             logger.info("Epoch {}/{}".format(epoch + 1, self.num_epochs))
             self.train_one_epoch()
-            
