b9d50952ab49f7c6c688ff4687da3ed6e1cccd83
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-25 20:00:11 +0100

    Init Trainer class

--- src/utils/training.py
+++ src/utils/training.py
@@ -6,10 +6,13 @@ import os
 import sys
 import logging
 from pathlib import Path
+from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -58,3 +61,45 @@ def get_loss_fn():
         Loss: PyTorch loss function.
     """
     return nn.BCEWithLogitsLoss()
+
+
+@dataclass
+class Trainer:
+    """
+    Class for training a model.
+    """
+    
+    model: nn.Module
+    optimizer: optim.Optimizer
+    scheduler: optim.lr_scheduler.LambdaLR
+    loss_fn: nn.Module
+    train_data_loader: DataLoader
+    valid_data_loader: DataLoader
+    device: torch.device
+    save_path: Path
+    num_epochs: int
+    num_training_steps: int
+    num_warmup_steps: int
+    log_step: int
+    save_step: int
+    max_grad_norm: float
+
+    def __post_init__(self):
+        """
+        Initialize trainer.
+        """
+        # Initialize tensorboard writer
+        self.writer = SummaryWriter(log_dir=self.save_path / "tensorboard")
+
+        # Initialize global step
+        self.global_step = 0
+
+    def fit(self):
+        """
+        Train model for specified number of epochs.
+        """
+        logger.info("Start training")
+        for epoch in range(self.num_epochs):
+            logger.info("Epoch {}/{}".format(epoch + 1, self.num_epochs))
+            self.train_one_epoch()
+            
