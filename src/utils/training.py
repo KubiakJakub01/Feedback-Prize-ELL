3ad116add7f30a90d1f4535aa948c280383c220d
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-25 19:49:05 +0100

    Import modules

--- src/utils/training.py
+++ src/utils/training.py
@@ -6,3 +6,10 @@ import os
 import sys
 import logging
 from pathlib import Path
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Set up logging
+logger = logging.getLogger(__name__)
