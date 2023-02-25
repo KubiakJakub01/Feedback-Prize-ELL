8aa220baa98348ddf386624b06e89ba410fa362d
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-25 19:50:35 +0100

    Init get_scheduler function

--- src/utils/training.py
+++ src/utils/training.py
@@ -27,3 +27,24 @@ def get_optimizer(model, lr):
         Optimizer: PyTorch optimizer.
     """
     return optim.Adam(model.parameters(), lr=lr)
+
+
+def get_scheduler(optimizer, num_warmup_steps, num_training_steps):
+    """
+    Get learning rate scheduler.
+
+    Args:
+        optimizer (Optimizer): PyTorch optimizer.
+        num_warmup_steps (int): Number of warmup steps.
+        num_training_steps (int): Number of training steps.
+
+    Returns:
+        Scheduler: PyTorch scheduler.
+    """
+    return optim.lr_scheduler.LambdaLR(
+        optimizer,
+        lr_lambda=lambda step: min(
+            1.0, step / num_warmup_steps
+        )
+        / (1.0 - step / num_training_steps),
+    )
