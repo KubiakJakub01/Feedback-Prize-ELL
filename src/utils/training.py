03551e9e5a9ade6d8ff2db5f551e2a09d29535d5
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-02-28 22:17:42 +0100

    Init model_utils

--- src/utils/training.py
+++ src/utils/training.py
@@ -19,49 +19,6 @@ from torch.utils.tensorboard import SummaryWriter
 logger = logging.getLogger(__name__)
 
 
-def get_optimizer(model, lr):
-    """
-    Get optimizer.
-
-    Args:
-        model (nn.Module): Model to optimize.
-        lr (float): Learning rate.
-
-    Returns:
-        Optimizer: PyTorch optimizer.
-    """
-    return optim.Adam(model.parameters(), lr=lr)
-
-
-def get_scheduler(optimizer, num_warmup_steps, num_training_steps):
-    """
-    Get learning rate scheduler.
-
-    Args:
-        optimizer (Optimizer): PyTorch optimizer.
-        num_warmup_steps (int): Number of warmup steps.
-        num_training_steps (int): Number of training steps.
-
-    Returns:
-        Scheduler: PyTorch scheduler.
-    """
-    return optim.lr_scheduler.LambdaLR(
-        optimizer,
-        lr_lambda=lambda step: min(1.0, step / num_warmup_steps)
-        / (1.0 - step / num_training_steps),
-    )
-
-
-def get_loss_fn():
-    """
-    Get loss function.
-
-    Returns:
-        Loss: PyTorch loss function.
-    """
-    return nn.BCEWithLogitsLoss()
-
-
 @dataclass
 class Trainer:
     """
