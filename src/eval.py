e843cd343a765c682e871ea8738cdb9dbe52114c
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-03-21 23:12:18 +0100

    Init eval.py module

---
+++ src/eval.py
@@ -0,0 +1,20 @@
+"""
+Module for evaluating the model
+"""
+# Import standard library
+import os
+import sys
+import logging
+import argparse
+from pathlib import Path
+from datetime import datetime
+
+# Import torch
+import torch
+
+# Import wandb
+import wandb
+
+
+if __name__ == "__main__":
+    pass
