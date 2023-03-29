89a9c0ba4dbd3e89f69810852f8c23759fe12e36
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-03-29 22:32:09 +0200

    Add get_args function

--- src/eval.py
+++ src/eval.py
@@ -23,5 +23,111 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
+def get_args():
+    """
+    Get arguments.
+    """
+    parser = argparse.ArgumentParser(description="Evaluate a model.")
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default=None,
+        help="Path to model.",
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default=None,
+        help="Name of model.",
+    )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default=None,
+        help="Path to data.",
+    )
+    parser.add_argument(
+        "--experiment_params",
+        type=str,
+        default=None,
+        help="Path to experiment parameters.",
+    )
+    parser.add_argument(
+        "--wandb",
+        type=bool,
+        default=False,
+        help="Whether to use wandb.",
+    )
+    parser.add_argument(
+        "--wandb_project",
+        type=str,
+        default="default",
+        help="Name of wandb project.",
+    )
+    parser.add_argument(
+        "--wandb_run_name",
+        type=str,
+        default=None,
+        help="Name of wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_tags",
+        type=str,
+        default=None,
+        help="Tags for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_entity",
+        type=str,
+        default=None,
+        help="Entity for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_group",
+        type=str,
+        default=None,
+        help="Group for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_notes",
+        type=str,
+        default=None,
+        help="Notes for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_config",
+        type=str,
+        default=None,
+        help="Config for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_resume",
+        type=bool,
+        default=False,
+        help="Whether to resume wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_id",
+        type=str,
+        default=None,
+        help="ID of wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_dir",
+        type=str,
+        default=None,
+        help="Directory for wandb run.",
+    )
+    parser.add_argument(
+        "--wandb_offline",
+        type=bool,
+        default=False,
+        help="Whether to run wandb offline.",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    pass    
+    # Parse arguments
+    param = get_args()    
