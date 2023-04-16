6c2d171da955aaf10259d481ad12777bf9990788
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-16 23:09:28 +0200

    Add Model class

--- src/utils/build_model.py
+++ src/utils/build_model.py
@@ -8,3 +8,38 @@ from transformers import AutoModelForSequenceClassification
 
 # Set up logging
 logger = logging.getLogger(__name__)
+
+
+class Model(nn.Module):
+    """
+    Model class.
+    """
+
+    def __init__(self, model_name: str, num_classes: int):
+        """
+        Initialize model.
+
+        Args:
+            model_name: Name of model.
+            num_classes: Number of classes.
+        """
+        super().__init__()
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name, num_labels=num_classes
+        )
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        """
+        Forward pass.
+
+        Args:
+            input_ids: Input ids.
+            attention_mask: Attention mask.
+            labels: Labels.
+
+        Returns:
+            Output: Model output.
+        """
+        return self.model(
+            input_ids=input_ids, attention_mask=attention_mask, labels=labels
+        )
