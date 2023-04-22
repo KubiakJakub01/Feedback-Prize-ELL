a9fa48b61a30e8cb193cf736bd07daedb2254288
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-22 22:15:04 +0200

    Init custom model

--- src/utils/build_model.py
+++ src/utils/build_model.py
@@ -147,56 +147,23 @@ class ConcatPooling(nn.Module):
         hidden = torch.cat([last_hidden_state[:, 0, :], last_hidden_state[:, -1, :]], dim=1)
         return hidden
 
-class Model(nn.Module):
-    """
-    Model class.
-    """
 
-    def __init__(self, model_name: str, num_classes: int):
-        """
-        Initialize model.
-
-        Args:
-            model_name: Name of model.
-            num_classes: Number of classes.
-        """
+class CustomModel(nn.Module):
+    def __init__(self, cfg) -> None:
         super().__init__()
+        self.cfg = cfg
         self.model = AutoModelForSequenceClassification.from_pretrained(
-            model_name, num_labels=num_classes
+            self.cfg.model_name
         )
 
-
-    def forward(self, input_ids, attention_mask, labels=None):
-        """
-        Forward pass.
-
-        Args:
-            input_ids: Input ids.
-            attention_mask: Attention mask.
-            labels: Labels.
-
-        Returns:
-            Output: Model output.
-        """
-        return self.model(
-            input_ids=input_ids, attention_mask=attention_mask, labels=labels
-        )
-
-    def save(self, path: str):
-        """
-        Save model.
-
-        Args:
-            path: Path to save model.
-        """
-        torch.save(self.state_dict(), path)
-
-    def load(self, path: str):
-        """
-        Load model.
-
-        Args:
-            path: Path to load model.
-        """
-        self.load_state_dict(torch.load(path))
+        if self.cfg.pooling == "mean":
+            self.pooling = MeanPooling()
+        elif self.cfg.pooling == "weighted":
+            self.pooling = WeightedLayerPooling(self.cfg.num_layers)
+        elif self.cfg.pooling == "lstm":
+            self.pooling = LSTMPooling(self.cfg.hidden_size)
+        elif self.cfg.pooling == "concat":
+            self.pooling = ConcatPooling(self.cfg.hidden_size)
+        else:
+            raise ValueError("Invalid pooling type")
 
