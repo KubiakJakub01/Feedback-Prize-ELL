e18daac66b2f75d77a28fa0edd4bb319c83d4826
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-10 22:25:12 +0200

    Add padding param

--- src/utils/params_parser.py
+++ src/utils/params_parser.py
@@ -135,6 +135,12 @@ class Hyperparameters:
             "help": "The maximum length of input sequences to use during training and inference."
         }
     )
+    padding = Literal["max_length", "longest"] = field(
+        metadata={
+            "help": "The type of padding to use for the input sequences. Defaults to max_length."
+        },
+        default="max_length",
+    )
     num_warmup_steps: int = field(
         metadata={
             "help": "The number of warmup steps to use for the scheduler. Defaults to 0."
