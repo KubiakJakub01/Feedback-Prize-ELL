ca2304b49ffa762046f8d9de71c83f289bd9633e
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-06-24 22:06:44 +0200

    Update basic logging config

--- src/train.py
+++ src/train.py
@@ -33,7 +33,7 @@ from utils.training import Trainer
 # Set up logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s",
+    format="%(asctime)s | %(levelname)s | %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
