f5083d1012d2b3b9cd7be19e3af6994e9c9abe96
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-06-30 22:21:53 +0200

    Add get_grade_from_predictions function

--- src/utils/inference_utils.py
+++ src/utils/inference_utils.py
@@ -5,12 +5,23 @@ import numpy as np
 GRADES = list(range(1, 5, 0.5))
 
 
-def get_grade_from_prediction(prediction: np.ndarray) -> float:
+def get_grade_from_prediction(prediction: float) -> float:
     """Get nearest grade from prediction
     
     Args:
-        prediction (np.ndarray): The prediction from the model.
+        prediction: The prediction from the model.
         
     Returns:
         float: The nearest grade from the prediction."""
     return GRADES[np.argmin(np.abs(GRADES - prediction))]
+
+
+def get_grade_from_predictions(predictions: np.ndarray) -> np.ndarray:
+    """Get nearest grade from predictions
+    
+    Args:
+        predictions (np.ndarray): The predictions from the model.
+        
+    Returns:
+        np.ndarray: The nearest grade from the predictions."""
+    return np.array([get_grade_from_prediction(prediction) for prediction in predictions])
