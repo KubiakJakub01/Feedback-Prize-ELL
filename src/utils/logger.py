35535b6ce41899164564992cfa345fdb14474d39
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-10 23:43:27 +0200

    Add logger utils moudle

---
+++ src/utils/logger.py
@@ -0,0 +1,33 @@
+import logging
+
+# Set up logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
+def log_debug(*args, **kwargs):
+    """Log an debug message."""
+    logger.debug(*args, **kwargs)
+
+
+def log_info(*args, **kwargs):
+    """Log an info message."""
+    logger.info(*args, **kwargs)
+
+
+def log_warning(*args, **kwargs):
+    """Log a warning message."""
+    logger.warning(*args, **kwargs)
+
+
+def log_error(*args, **kwargs):
+    """Log an error message."""
+    logger.error(*args, **kwargs)
